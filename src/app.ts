f44c321fd9ddabcb555acc01430d53e23170081b
Nazmul Hoque <sujon686500@gmail.com>
2024-11-23 21:02:57 +0600

    project setup

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+//application routes
+
+const getAController = (req: Request, res: Response) => {
+  res.send('Hello World!');
+};
+
+app.get('/', getAController);
+
+export default app;
